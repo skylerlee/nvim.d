8d07ff44c1f7f1c6ecb685af4905890f36547ee2
skylerlee <skyler.ac.lee@gmail.com>
2023-09-21 00:27:00 +0800

    Init project

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
+-- bootstrap lazy.nvim
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -12,3 +13,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({})
